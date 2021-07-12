e107d9d52db64566783fcb71efca2c77962cd13a
gohardsun <ahnmyungsun95@gmail.com>
2021-07-12 21:57:20 +0900

    백준 단계별풀기 7.문자열 -1. 아스키코드 출력

---
+++ 07.string/01.asciicode.cpp
@@ -0,0 +1,16 @@
+//백준 11654번 아스키코드
+//
+
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    char n;
+    cin >> n;
+
+    cout << (int)n
+            ;
+}
